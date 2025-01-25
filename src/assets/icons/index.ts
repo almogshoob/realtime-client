45b1b998ac9324496ca99369a78099ac6b8f6ed9
Almog Shoob <almogshoob1@gmail.com>
2025-01-25 19:22:42 +0200

    update server url, remove unused components

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,7 +2,6 @@
 import CloseIcon from "./close.svg?react";
 import AddIcon from "./plus.svg?react";
 import RefreshIcon from "./refresh.svg?react";
-import SettingsIcon from "./settings.svg?react";
 import StopIcon from "./stop.svg?react";
 
-export { AddIcon, CloseIcon, RefreshIcon, SettingsIcon, StopIcon };
+export { AddIcon, CloseIcon, RefreshIcon, StopIcon };
