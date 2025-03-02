2607062504a02840091812c80abc494a2094123f
Almog Shoob <almogshoob1@gmail.com>
2025-03-02 16:15:16 +0200

    change URL back to prod

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 import { Coordinate } from "../types";
 
-export const SERVER_URL = "http://localhost:3001";
-// export const SERVER_URL = "https://realtime-server-jc2r.onrender.com";
+// export const SERVER_URL = "http://localhost:3001";
+export const SERVER_URL = "https://realtime-server-jc2r.onrender.com";
 export const API_URL = "https://app.busnearby.co.il";
 export const EARTH_RADIUS_KM = 6371;
 export const DEFAULT_LOCATION: Coordinate = { lat: 32.085023, lon: 34.785401 };
