6d4aacf69858b75d6e455d5afe1265e67596fcfd
Almog Shoob <almogshoob1@gmail.com>
2025-06-02 23:48:20 +0300

    pwa

---
+++ sw.js
@@ -0,0 +1,8 @@
+// minimal requirements for installable pwa: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Making_PWAs_installable
+// guide to functional sw: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Tutorials/js13kGames/Offline_Service_workers
+// guide to use vite plugin: https://vite-pwa-org.netlify.app/guide/
+// youtube pwa guide: https://www.youtube.com/playlist?list=PL4cUxeGkcC9gTxqJBcDmoi5Q2pzDusSL7 
+
+self.addEventListener("install", (e) => { });
+
+self.addEventListener("fetch", (e) => { });
